cefa1762f5b561a6c3301fe8335a9a4c26599270
Mark Rudolph <mark@k8ty.app>
2023-07-06 23:55:27 -0400

    add some scaladoc

--- test-opinions/src/main/scala/test/opinions/Assertions.scala
+++ test-opinions/src/main/scala/test/opinions/Assertions.scala
@@ -4,6 +4,12 @@ import zio.*
 import zio.test.*
 import opinions.*
 
-extension[A](a: A)
+extension [A](a: A)
+
+  /** Wrap a: A in Assertion.equalTo(a)
+    */
   def eqTo: Assertion[A] = Assertion.equalTo(a)
+
+  /** Wrap a: A in Assertion.not(Assertion.equalTo(a))
+    */
   def neqTo: Assertion[A] = Assertion.not(a.eqTo)
--- test-opinions/src/main/scala/test/opinions/Expectations.scala
+++ test-opinions/src/main/scala/test/opinions/Expectations.scala
@@ -4,5 +4,11 @@ import zio.*
 import zio.mock.*
 
 extension [A: Tag](a: A)
-  def expected: Result[Any, Nothing, A]  = Expectation.value(a)
+
+  /** Wrap a: A in Expectation.value(a)
+    */
+  def expected: Result[Any, Nothing, A] = Expectation.value(a)
+
+  /** Wrap a: A in Expectation.failure(a)
+    */
   def expectedF: Result[Any, A, Nothing] = Expectation.failure(a)
